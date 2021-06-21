ae8005567ff97e234c1b0785dff0bd31abaaddf4
Meawhung <meawhung@gmail.com>
2021-06-21 15:54:45 +0900

    add all route

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -1,5 +1,7 @@
 import json
 
+import pytest
+
 
 def test_create_summary(test_app_with_db):
     response = test_app_with_db.post(
@@ -134,7 +136,7 @@ def test_update_summary_incorrect_id(test_app_with_db):
     assert response.json()["detail"] == "Summary not found"
 
     response = test_app_with_db.put(
-        f"/summaries/0/",
+        "/summaries/0/",
         data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
     )
     assert response.status_code == 422
@@ -220,55 +222,3 @@ def test_update_summary_invalid_url(test_app):
     )
     assert response.status_code == 422
     assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
-
-
-# def test_update_summary_invalid_json(test_app_with_db):
-#     response = test_app_with_db.post(
-#         "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-#     )
-#     summary_id = response.json()["id"]
-
-#     response = test_app_with_db.put(f"/summaries/{summary_id}/", data=json.dumps({}))
-#     assert response.status_code == 422
-#     assert response.json() == {
-#         "detail": [
-#             {
-#                 "loc": ["body", "url"],
-#                 "msg": "field required",
-#                 "type": "value_error.missing",
-#             },
-#             {
-#                 "loc": ["body", "summary"],
-#                 "msg": "field required",
-#                 "type": "value_error.missing",
-#             },
-#         ]
-#     }
-
-
-# def test_update_summary_invalid_keys(test_app_with_db):
-#     response = test_app_with_db.post(
-#         "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-#     )
-#     summary_id = response.json()["id"]
-
-#     response = test_app_with_db.put(
-#         f"/summaries/{summary_id}/", data=json.dumps({"url": "https://foo.bar"})
-#     )
-#     assert response.status_code == 422
-#     assert response.json() == {
-#         "detail": [
-#             {
-#                 "loc": ["body", "summary"],
-#                 "msg": "field required",
-#                 "type": "value_error.missing",
-#             }
-#         ]
-#     }
-
-#     response = test_app_with_db.put(
-#         f"/summaries/{summary_id}/",
-#         data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
-#     )
-#     assert response.status_code == 422
-#     assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
